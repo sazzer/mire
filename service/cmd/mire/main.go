7a5f95c7b1490aef2077bff1d7f9da22476befa0
Graham Cox <graham@grahamcox.co.uk>
2020-07-15 18:17:54 +0100

    Command line application with logging

---
+++ service/cmd/mire/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+	log.Logger = log.With().Caller().Logger()
+
+	log.Info().Msg("Starting Mire...")
+}
--- service/go.mod
+++ service/go.mod
@@ -1,3 +1,8 @@
 module github.com/sazzer/mire/service
 
 go 1.14
+
+require (
+	github.com/apex/log v1.6.0
+	github.com/rs/zerolog v1.19.0
+)
