713cae35c7c016a53f944f6682f13dbab54f1126
Graham Cox <graham@grahamcox.co.uk>
2020-07-16 07:58:25 +0100

    Start an Echo server listening

--- service/go.mod
+++ service/go.mod
@@ -5,5 +5,12 @@ go 1.14
 require (
 	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/rs/zerolog v1.19.0
+	github.com/valyala/fasttemplate v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
--- service/internal/infrastructure/server/server.go
+++ service/internal/infrastructure/server/server.go
@@ -1,17 +1,41 @@
 package server
 
-import "github.com/rs/zerolog/log"
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog/log"
+)
 
 // Server represents the actual web server that does the work
 type Server struct {
+	server *echo.Echo
 }
 
 // New creates a new web server
 func New() Server {
-	return Server{}
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.RequestID())
+
+	return Server{
+		server: e,
+	}
 }
 
 // Start will start the service listening on the given port
 func (s Server) Start(port uint16) {
 	log.Info().Uint16("port", port).Msg("Starting server")
+
+	err := s.server.Start(fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to start server")
+	}
 }
