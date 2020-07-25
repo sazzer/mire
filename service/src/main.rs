bec5d5cdc3182d44233f2814a26ad5a27b28f7be
Graham Cox <graham@grahamcox.co.uk>
2020-07-25 10:09:16 +0100

    Set up logging and loading config from the environment

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,8 +1,34 @@
-use dotenv;
+use config::{Config, Environment};
+use dotenv::dotenv;
+use serde::Deserialize;
+
+/// Representation of the application settings that will be loaded from the environment
+#[derive(Debug, Deserialize)]
+struct Settings {}
+
+impl Default for Settings {
+    /// Construct the settings from the environment
+    ///
+    /// # Returns
+    /// The Settings object, loaded from the environment variables
+    fn default() -> Self {
+        let mut s = Config::new();
+        s.merge(Environment::default())
+            .expect("Failed to load environment properties");
+
+        s.try_into().expect("Failed to build settings from config")
+    }
+}
 
 #[actix_rt::main]
 async fn main() {
-    let _ = dotenv::dotenv();
+    dotenv().ok();
+
+    tracing_subscriber::fmt::init();
+
+    let settings = Settings::default();
+
+    log::debug!("Application settings: {:?}", settings);
 
     mire_lib::main().await
 }
