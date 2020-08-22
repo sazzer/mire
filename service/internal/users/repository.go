b44400a4e2ca53e670ff1da8b703a5ec5dca9b53
Graham Cox <graham@grahamcox.co.uk>
2020-08-22 13:45:44 +0100

    WIP User Repository

--- service/internal/users/repository.go
+++ service/internal/users/repository.go
@@ -1,6 +1,10 @@
 package users
 
 import (
+	"context"
+	"time"
+
+	"github.com/rs/zerolog/log"
 	"github.com/sazzer/mire/service/internal/database"
 )
 
@@ -14,4 +18,39 @@ func newRepository(db database.Database) repository {
 	return repository{
 		db: db,
 	}
+}
+
+type userRecord struct {
+	ID              UserID    `db:"user_id"`
+	Version         string    `db:"version"`
+	Created         time.Time `db:"created"`
+	Updated         time.Time `db:"updated"`
+	Email           Email     `db:"email"`
+	DisplayName     string    `db:"display_name"`
+	Authentications string    `db:"authentications"`
+}
+
+func (u userRecord) UserModel() UserModel {
+	return UserModel{
+		ID:      u.ID,
+		Version: u.Version,
+		Created: u.Created,
+		Updated: u.Updated,
+		UserData: UserData{
+			Email:           u.Email,
+			DisplayName:     u.DisplayName,
+			Authentications: []Authentication{},
+		},
+	}
+}
+
+// Get the single user that has the provided unique ID.
+func (r repository) GetUserByID(ctx context.Context, userID UserID) UserModel {
+	var user userRecord
+
+	if err := r.db.QueryOne(ctx, &user, "SELECT * FROM users WHERE user_id = $1", &userID); err != nil {
+		log.Info().Err(err).Interface("user_id", userID).Msg("Failed to find user")
+	}
+
+	return user.UserModel()
 }
