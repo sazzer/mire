b2fd3c848b3961da16c25c00decc43651c2ce9d3
Graham Cox <graham@grahamcox.co.uk>
2020-08-26 17:41:20 +0100

    Some a11y tweaks

--- webapp/src/header/usermenu.tsx
+++ webapp/src/header/usermenu.tsx
@@ -5,18 +5,21 @@ import React from "react";
  * The User Menu dropdown appearing in the header bar.
  */
 export const UserMenu: React.FC = () => {
+  /* eslint-disable jsx-a11y/anchor-is-valid */
   return (
     <li className="nav-item dropdown" role="menu">
-      <span
+      <a
+        href="#"
         className="nav-link dropdown-toggle"
         id="userMenuDropdown"
         role="button"
         data-toggle="dropdown"
         aria-haspopup="true"
         aria-expanded="false"
+        aria-label="User Menu for Graham"
       >
         Graham
-      </span>
+      </a>
       <div
         className="dropdown-menu dropdown-menu-right"
         aria-labelledby="userMenuDropdown"
--- webapp/src/landing/index.tsx
+++ webapp/src/landing/index.tsx
@@ -6,15 +6,16 @@ import landingPicture from "./landing.jpg";
  * The landing page, containing the available login options.
  */
 export const LandingPage: React.FC = () => {
+  /* eslint-disable jsx-a11y/alt-text */
   return (
     <div className="row">
-      <aside className="col-12 col-lg-3 order-lg-3">
+      <div className="col-12 col-lg-3 order-lg-3">
         <Authentication />
-      </aside>
+      </div>
       <div className="col-12 col-lg-9">
         <img
           src={landingPicture}
-          alt="Mire"
+          role="presentation"
           className="img-fluid img-thumbnail rounded shadow"
         />
       </div>
