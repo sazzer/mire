f18581b6841e85ea2410f90ce55b543a6a390ea8
Graham Cox <graham@grahamcox.co.uk>
2020-09-04 16:30:29 +0100

    Support for resetting the database between tests

--- e2e/cypress/plugins/index.js
+++ e2e/cypress/plugins/index.js
@@ -1,8 +1,14 @@
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const database = require("./database");
 
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on) => {
+module.exports = (on, config) => {
+  database.openPool(config.env.POSTGRES_URL);
+  on("task", {
+    "db:reset": database.reset,
+    "db:seed": database.seed,
+  });
   on("file:preprocessor", cucumber());
 };
