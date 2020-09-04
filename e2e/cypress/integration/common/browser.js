2e8f0c3483fe934193e474024bf18c52557806b1
Graham Cox <graham@grahamcox.co.uk>
2020-09-04 17:05:50 +0100

    Clear session storage between tests

--- e2e/cypress/integration/common/browser.js
+++ e2e/cypress/integration/common/browser.js
@@ -1,5 +1,9 @@
 import { Before } from "cypress-cucumber-preprocessor/steps";
 
 Before(() => {
-  cy.visit("/");
+  cy.visit("/", {
+    onBeforeLoad: (win) => {
+      win.sessionStorage.clear();
+    },
+  });
 });
